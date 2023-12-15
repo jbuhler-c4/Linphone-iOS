6d4be81e1486ead92a1957bf2973b0da196c9440
Joshua Buhler <josh.buhler@snapone.com>
2023-12-15 14:01:12 -0700

    Trying to zip up all of the linphone junk

--- Package.swift
+++ Package.swift
@@ -9,13 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Linphone-iOS",
-            targets: ["Linphone-iOS"]),
+            targets: ["Linphone-iOS", "LinphoneSDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Linphone-iOS"),
+        .binaryTarget(name: "LinphoneSDK",
+                      url: "https://github.com/jbuhler-c4/Linphone-iOS/releases/download/5.1.63/LinphoneSDK.zip",
+                     checksum: "92af7ba71280cbcf3e34864bcc5ccc0fdbcde53b686833cb38002c86a766171f"),
         .testTarget(
             name: "Linphone-iOSTests",
             dependencies: ["Linphone-iOS"]),
