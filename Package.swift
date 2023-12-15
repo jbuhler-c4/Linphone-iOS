957d8bafb876139a669e44524c7403896d30a8b3
Joshua Buhler <josh.buhler@snapone.com>
2023-12-15 15:28:12 -0700

    Maybe we need to do these individually?

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Linphone-iOS",
-            targets: ["Linphone-iOS", "LinphoneSDK"]),
+            targets: ["Linphone-iOS", "linphone"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Linphone-iOS"),
-        .binaryTarget(name: "LinphoneSDK",
-                      url: "https://github.com/jbuhler-c4/Linphone-iOS/releases/download/5.1.63/LinphoneSDK.zip",
-                     checksum: "92af7ba71280cbcf3e34864bcc5ccc0fdbcde53b686833cb38002c86a766171f"),
+        .binaryTarget(name: "linphone",
+                      url: "https://github.com/jbuhler-c4/Linphone-iOS/releases/download/5.1.65/linphone.xcframework.zip",
+                     checksum: "ce7b435d732bff3dfba8944aa8e33939d9e42a04b34daa84f4ebbe906c68f27d"),
         .testTarget(
             name: "Linphone-iOSTests",
             dependencies: ["Linphone-iOS"]),
